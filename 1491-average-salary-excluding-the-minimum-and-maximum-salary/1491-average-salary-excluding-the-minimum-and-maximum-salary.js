421cd15a4154b2c153701c1a74ff742ae8b2f4e4
Fatuma <57506951+fatuma-saleh@users.noreply.github.com>
2022-04-05 12:30:35 -0400

    Time: 79 ms (50.32%), Space: 42 MB (59.91%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.js
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.js
@@ -3,13 +3,12 @@
  * @return {number}
  */
 var average = function(salary) {
-    let max = salary[0],min = salary[0],maxPosition,minPosition,sum = 0,avg,a,b
+    let sum = 0,avg,a,b
     
     salary.sort((a,b) => a-b)
     salary.pop()
     salary.shift()
     
-   
     for(let k = 0 ;k<= salary.length-1;k++){
         sum += salary[k]
     }
