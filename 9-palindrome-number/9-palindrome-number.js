7fd78aee00e5328036c846e0bda3e5f679321f42
Fatuma <57506951+fatuma-saleh@users.noreply.github.com>
2022-04-13 17:16:03 -0400

    Time: 180 ms (83.49%), Space: 51.6 MB (21.48%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+   
+    let str = x.toString()
+    let rev = str.split("").reverse().join("")
+    if(str === rev){
+        return true
+    }
+      
+    return false
+    
+};
