6096d9c8fb8465e8255384d5d32f0154b47e42b6
Fatuma <57506951+fatuma-saleh@users.noreply.github.com>
2022-08-08 10:57:35 -0400

    Time: 58 ms (98.18%), Space: 43.5 MB (8.08%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -3,10 +3,10 @@
  * @return {number[]}
  */
 var runningSum = function(nums) {
-    let arr=[];
-    let sum=0
-    for(let i=0;i< nums.length;i++){
-       sum =sum+nums[i]
+    let arr=[],sum=0;
+    
+    for(let num of nums){
+       sum =sum+num
         arr.push(sum)
     }
     return arr
