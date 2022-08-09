97b59ab53b01d55e8362df16e8f0a9d668d7a16c
Fatuma <57506951+fatuma-saleh@users.noreply.github.com>
2022-08-09 14:34:58 -0400

    Time: 72 ms (97.24%), Space: 42 MB (95.37%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.js
+++ 205-isomorphic-strings/205-isomorphic-strings.js
@@ -4,9 +4,9 @@
  * @return {boolean}
  */
 var isIsomorphic = function(s, t) {
-    if(s.length!==t.length){
-        return false
-    }
+    // if(s.length!==t.length){
+    //     return false
+    // }
     for(let i=0;i<s.length;i++){
         if(s.indexOf(s[i])!==t.indexOf(t[i])) return false
     }
