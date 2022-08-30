3d56747fd3b39987e9b2373edec7df0358746712
Fatuma <57506951+fatuma-saleh@users.noreply.github.com>
2022-08-30 14:10:16 -0400

    Time: 109 ms (19.20%), Space: 42 MB (98.55%) - LeetHub

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+  
+    for(let i = 0;i< nums.length;i++){
+        if(nums[i] === target){
+           let pos = i
+        
+        while(pos < nums.length && nums[pos]===target)
+            pos++
+        return [ i, pos-1 ]
+        }
+    }
+        return[ -1,-1 ]
+};
