15f562bc4ec0c28d40d02c8329de0cb48abbf65f
Fatuma <57506951+fatuma-saleh@users.noreply.github.com>
2023-01-30 22:29:40 -0500

    Time: 88 ms (49.96%), Space: 46.5 MB (41.89%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -5,6 +5,5 @@
 var isPalindrome = function(s) {
    const reg = new RegExp(/[^a-z0-9]/, 'g');
     const str = s.toLowerCase().replace(reg, '');
-    const newArr = str.split('');
-    return str === newArr.reverse().join('');
+    return str === str.split('').reverse().join('');
 };
